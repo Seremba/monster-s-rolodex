bcf9c4e34ecc235e36d0a2ef7c5acf3081623bb1
SerembaPatrick <pserembae@gmail.com>
2019-10-03 18:29:18 +0300

    Updates

---
+++ precache-manifest.3a4ef3be62cb739922364a633b4fcdb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8842a4b6515735118511bdc3e92b2063",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "765a194a0fd87d83dee3",
+    "url": "/monsters-rolodex/static/css/main.376deb74.chunk.css"
+  },
+  {
+    "revision": "877ab33c4cabc00823c7",
+    "url": "/monsters-rolodex/static/js/2.4586650f.chunk.js"
+  },
+  {
+    "revision": "765a194a0fd87d83dee3",
+    "url": "/monsters-rolodex/static/js/main.0702c6f2.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
